f3d630345ce0f0c9cf059beacbd6db0cead1c92d
daveoa10 <damidave7@outlook.com>
2025-09-25 23:02:25 -0400

    Add files via upload

---
+++ info.py
@@ -0,0 +1,87 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/me.jpeg"
+#about_me = "I'm SpongeBob SquarePants. I work at the Krusty Krab and I make fantastic Krabby Patties. "
+about_me = "I'm David Ashade, a student at the Georgia Institute of Technology."
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "www.linkedin.com/in/david-ashade"
+my_github_url = "https://github.com/daveoa10"
+#my_email_address = "spongebob@gmail.com"
+my_email_address = "dashade3@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Biology',
+    #Bachelor of Science in Biology
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2029',
+    #2029
+    'GPA':'3.9'
+    #3.9
+}
+course_data = {
+    "code":["CS 1301", "MATH 1551", "BIOS 1207", "APPH 1040", "GT 1000"],
+    #["CS 1301, MATH 1551, BIOS 1207, APPH 1040, GT 1000"]
+    "names":["Intro to CS", "Differential Calculus", "Intro to Biology", "Wellness" ,"Freshman Seminar"],
+    #["Intro to CS", "Differential Calculus", "Intro to Biology", "Wellness" ,"Freshman Seminar"]
+    "semester_taken":["1st","1st","1st","1st","1st"],
+    #["1st","1st","1st","1st","1st"]
+    "skills":["I know some Python and some Java.", "I can do basic calculus.", "I know basic ecology.", "I know how to take care of myself.", "I know my way around GT (somewhat)."],
+    }
+    #["I know some Python and some Java", "I can do basic calculus", "I know basic ecology", "I know how to take care of myself", "I know my way around GT"]
+experience_data = {
+    "High School Student": (["- Worked on getting all A's",
+                                                                          "- Got good enough grades to get a scholarship by my high school", "- Made my parents proud"],"Images/grad.jpg"),
+    "Big Brother":(["- Helped raise my ten year old brother",
+                                                           "- Fed and clothed him and played with him"],"Images/us.jpg"),
+    #"Jelly Fish Catcher":(["- Caught 100+ Jellyfish with the help of my colleague Patrick"],"Images/jelly.jpg")
+    #"Wor
+
+}
+
+projects_data = {
+    "Group Project": "Proposed an app to help patients across Georgia with sickle cell anemia",
+    #"Group Project: "Proposed an app to help patients across Georgia with sickle cell anemia",
+    "Questionnaire": "Made a survey to ask students about their opinions on the dining hall",
+}
+
+programming_data = {
+    "Python": 65,
+    "Java": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    
+}
+spoken_icons = {
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Spanish": "Intermediate",
+    #"Spanish: "Intermediate",
+}
+leadership_data = {
+    "Leadership Program": (["- Graduated from the 2024 Columbia County Youth Leadership Program"],"Images/ccyl.png"),
+    #Graduated from the 2024 Columbia County Youth Leadership Program
+
+}
+activity_data={
+    "Varsity Soccer": ["- Scored 18 goals last season", "- Got second in region"]
+}
+#   "Varsity Soccer": ["- Scored 18 goals last season", "- Got second in region"]
